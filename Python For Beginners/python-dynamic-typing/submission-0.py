427fca36781b0820edb0ce085ca4682623f56573
Ignacio Nevarez Zuñiga <55265068+IgnacioNevarez@users.noreply.github.com>
2026-04-28 12:08:37 -0600

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 12
+print(type(variable))
+variable = 12.5
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "Testing"
+print(type(variable))
+variable = [1, 5, 9]
+print(type(variable))
