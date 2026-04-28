a0ceebc12325397b3c28eacd1cd5e0d349abb602
Ignacio Nevarez Zuñiga <55265068+IgnacioNevarez@users.noreply.github.com>
2026-04-28 11:34:30 -0600

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,12 @@
+integer_type = 33
+float_type = 3.14
+bool_type = True
+string_type = 'Ignacio'
+list_type = [1, 5, 9]
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
